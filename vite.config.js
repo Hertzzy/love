3648138607277067565cbb4e8d10f98602b0cf85
Hertzy <helter.xavier@hotmail.com>
2024-12-20 02:06:05 -0300

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/welove'
+  base: '/' // Base padrão para sites hospedados na raiz do domínio
 });
